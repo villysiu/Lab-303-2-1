d5bc4f67868cc3d1a950673329214698dbac584d
Villy Siu <villysiu@gmail.com>
2024-11-14 07:29:43 -0800

    Initial commit

---
+++ src/AssignmentOperatorsDemo.java
@@ -0,0 +1,16 @@
+public class AssignmentOperatorsDemo{
+    public static void main(String[] args) {
+
+        int j, k;
+        j = 10;
+        j = 5;
+        k = j;
+        System.out.println("j is :" + j);
+        System.out.println("k is :" + k);
+
+        k = j = 10;
+        System.out.println("j is :" + j);
+        System.out.println("k is :" + k);
+
+    }
+}
---
+++ src/UnaryOperator.java
@@ -0,0 +1,15 @@
+public class UnaryOperator {
+    public static void main(String[] args) {
+        int sum = +1;
+        System.out.println(sum);
+        sum--;
+        System.out.println(sum);
+        sum++;
+        System.out.println(sum);
+        sum = -sum;
+        System.out.println(sum);
+        boolean result = false;
+        System.out.println(result);
+        System.out.println(!result);
+    }
+}
